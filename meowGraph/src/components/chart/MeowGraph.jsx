56cee783de5e9b8efa16c9e75392922a62b41029
Leenuli <leena.karjalainen92@gmail.com>
2023-10-15 12:02:58 +0300

    adding audio, in project

--- meowGraph/src/components/chart/MeowGraph.jsx
+++ meowGraph/src/components/chart/MeowGraph.jsx
@@ -12,6 +12,16 @@ import lol from "../../images/lol.PNG";
 import madf from "../../images/mad.PNG";
 import smile from "../../images/smile.PNG";
 import wut from "../../images/wut.PNG";
+/* SOUND */
+import useSound from "use-sound";
+import { default as amazingSfx } from "../../sounds/mp3/amazing.mp3";
+import { default as boringSfx } from "../../sounds/mp3/boring.mp3";
+import { default as brilliantSfx } from "../../sounds/mp3/brilliant.mp3";
+import { default as bummerSfx } from "../../sounds/mp3/bummer.mp3";
+import { default as bungeeSfx } from "../../sounds/mp3/bungee.mp3";
+import { default as byeByeSfx } from "../../sounds/mp3/byebye.mp3";
+import { default as collectSfx } from "../../sounds/mp3/collect.mp3";
+import { default as comeonthenSfx } from "../../sounds/mp3/comeonthen.mp3";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function MeowGraph() {
@@ -21,10 +31,21 @@ function MeowGraph() {
   const [okCat, setOkCat] = useState(1);
   const [aaaCat, setAaaCat] = useState(0);
   const [omgCat, setOmgCat] = useState(0);
-
   const [wutCat, setWutCat] = useState(0);
   const [smileCat, setSmileCat] = useState(0);
 
+  /* sound */
+
+  const [byebye] = useSound(byeByeSfx, { volume: 0.25 });
+  const [amazing] = useSound(amazingSfx, { volume: 0.25 });
+  const [brilliant] = useSound(brilliantSfx, { volume: 0.25 });
+  const [bummer] = useSound(bummerSfx, { volume: 0.25 });
+  const [bungee] = useSound(bungeeSfx, { volume: 0.25 });
+  const [boring] = useSound(boringSfx, { volume: 0.25 });
+  const [collect] = useSound(collectSfx, { volume: 0.25 });
+  const [comeonthen] = useSound(comeonthenSfx, { volume: 0.25 });
+
+  /* DATA */
   const data = {
     datasets: [
       {
@@ -79,37 +100,46 @@ function MeowGraph() {
   };
 
   const happy = () => {
+    byebye();
     setHappyCat((happyCat) => happyCat + 1);
+
     console.log(happyCat);
   };
   const sad = () => {
+    amazing();
     setSadCat((sadCat) => sadCat + 1);
   };
   const mad = () => {
+    boring();
     setMadCat((madCat) => madCat + 1);
   };
   const ok = () => {
+    bummer();
     setOkCat((okCat) => okCat + 1);
   };
 
   const aaah = () => {
+    bungee();
     setAaaCat((aaaCat) => aaaCat + 1);
   };
   const omgash = () => {
+    collect();
     setOmgCat((omgCat) => omgCat + 1);
   };
 
   const wuts = () => {
+    brilliant();
     setWutCat((wutCat) => wutCat + 1);
   };
   const smiled = () => {
+    comeonthen();
     setSmileCat((smileCat) => smileCat + 1);
   };
 
   return (
     <Grid container>
       <Grid item xs={12} md={12} xl={12}>
-        <h1>Valitse kissa kuva ja kerro fiiliksesi</h1>
+        <h1>How meow you are today? pick a cat</h1>
       </Grid>
       <Grid item xs={12} md={6} xl={12}>
         {okCat == 0 ? (
@@ -161,14 +191,12 @@ function MeowGraph() {
         >
           <Avatar alt="DUH" src={genious} sx={{ width: 100, height: 100 }} />
         </button>
-
         <button
           onClick={omgash}
           style={{ backgroundColor: "#ff6d00", margin: "5px" }}
         >
           <Avatar alt="AAA" src={omg} sx={{ width: 100, height: 100 }} />
         </button>
-
         <button
           onClick={wuts}
           style={{ backgroundColor: "#18ffff", margin: "5px" }}
